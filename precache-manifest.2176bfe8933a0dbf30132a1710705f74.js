55ee1952c8a5785fcb42db428243efaf06e8f352
mrKrastev <56829655+mrKrastev@users.noreply.github.com>
2020-06-17 14:34:22 +0100

    Updates

---
+++ precache-manifest.2176bfe8933a0dbf30132a1710705f74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69fd8ec705b987f564d971d93e7ae9cd",
+    "url": "/quick-maths-react/index.html"
+  },
+  {
+    "revision": "dac03983d44fdf751e8f",
+    "url": "/quick-maths-react/static/css/main.5baef382.chunk.css"
+  },
+  {
+    "revision": "67ea9692f18d14221690",
+    "url": "/quick-maths-react/static/js/2.6aa9e9e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quick-maths-react/static/js/2.6aa9e9e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac03983d44fdf751e8f",
+    "url": "/quick-maths-react/static/js/main.a4ea201d.chunk.js"
+  },
+  {
+    "revision": "f6bc6b9d3f4e9f4c01bc",
+    "url": "/quick-maths-react/static/js/runtime-main.db212529.js"
+  },
+  {
+    "revision": "60ec67d5a53e14043bfd616606600a26",
+    "url": "/quick-maths-react/static/media/percentagelogo.60ec67d5.png"
+  }
+]);
